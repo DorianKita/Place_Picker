0d7f86022f0820c9af1f699d4142bcd37f521a0d
Dorian Kita <dorian.kita90@gmail.com>
2024-02-26 21:37:15 +0100

    added function to remove selected item from local storage

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,13 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem(
+      "selectedPlaces",
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
+    console.log(selectedPlace.current);
   }
 
   return (
